718383dda688d30649637ec7d7ad07762043dbc8
Aleksandra Bondzic <dragonqueen2all@roman-pc.gateway.2wire.net>
2012-08-04 22:23:03 -0500

    Initial Import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LivingDeadReviews::Application.config.secret_token = 'e3681a14d46aac8d8716ee7072ae43efdddd92c628a55cd01684cc020346f9fa7b9fe31e89b40f2c600c597b062a010c01133ad493c4e1eed6060173f7466248'
